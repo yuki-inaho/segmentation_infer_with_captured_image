1dd489384d381ce1e63b43bc741387bb195c2c4c
yuki-inaho <yoshikawa@inaho.co>
2020-10-26 22:09:38 +0900

    Add a function to run batch process

--- infer.py
+++ infer.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 from scripts.utils import (
     create_inference,
@@ -9,6 +10,7 @@ from pathlib import Path
 import numpy as np
 from scripts.rgb_manager import RGBImageManager
 from scripts.utils import get_overlay_rgb_image
+from tqdm import tqdm
 
 SCRIPT_DIR = Path(__file__).parent.resolve()
 
@@ -26,6 +28,13 @@ def infer_image(image, inference):
     return mask_image
 
 
+def get_image_pathes(input_data_dir):
+    image_pathes = Path(input_data_dir).glob("*.png")
+    image_path_list = [str(image_path) for image_path in image_pathes]
+    return image_path_list
+
+
+
 @click.command()
 @click.option("--toml-path", "-t", default=f"{SCRIPT_DIR}/cfg/dualzense_r.toml")
 @click.option("--input-data-dir", "-i", default=f"{SCRIPT_DIR}/data")
@@ -34,15 +43,19 @@ def main(toml_path, input_data_dir, output_data_dir):
     rgb_manager = RGBImageManager(toml_path)
     # = get_dataset(input_data_dir)
     inference = create_inference()
+    if not os.path.exists(output_data_dir):
+        os.makedirs(output_data_dir)
 
-    file_name = f"{SCRIPT_DIR}/data/2020-10-09_05-31-30_DUALZENSE_R_r_see_0.png"
-    base_name = Path(file_name).name
-    rgb_image = get_image(file_name, rgb_manager)
-    segmentation_mask = infer_image(rgb_image, inference)
-    rgb_image_masked = get_overlay_rgb_image(rgb_image, segmentation_mask)
+    image_path_list = get_image_pathes(input_data_dir)
 
-    cv2.imwrite(f"{output_data_dir}/{base_name}", rgb_image_masked)
-    cv2.waitKey(10)
+    for image_path in tqdm(image_path_list):
+        base_name = Path(image_path).name
+        rgb_image = get_image(image_path, rgb_manager)
+        segmentation_mask = infer_image(rgb_image, inference)
+        rgb_image_masked = get_overlay_rgb_image(rgb_image, segmentation_mask)
+
+        cv2.imwrite(f"{output_data_dir}/{base_name}", rgb_image_masked)
+        cv2.waitKey(10)
 
 if __name__ == "__main__":
     main()
